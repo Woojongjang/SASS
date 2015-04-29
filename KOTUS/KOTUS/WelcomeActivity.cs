3fc9af1bd429620514c99367a3d5260339acf256
Nicholas G <nlg@ucsd.edu>
2015-04-29 14:48:06 -0700

    NLG: finished welcome screen template

--- KOTUS/KOTUS/WelcomeActivity.cs
+++ KOTUS/KOTUS/WelcomeActivity.cs
@@ -13,6 +13,11 @@ namespace KOTUS
 	public class WelcomeActivity : Activity
 	{
 
+		private EditText user_email_field;
+		private EditText user_password_field;
+		private Button log_in_button;
+		private TextView sign_me_up_link;
+
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
@@ -20,6 +25,23 @@ namespace KOTUS
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
 
+
+			// find the layout objects
+			user_email_field = FindViewById<EditText> (Resource.Id.editText1);
+			user_password_field = FindViewById<EditText> (Resource.Id.editText2);
+			log_in_button = FindViewById<Button> (Resource.Id.button1);
+			sign_me_up_link = FindViewById<TextView> (Resource.Id.textView3);
+
+
+			// designate what to do on button and link clicks
+
+			log_in_button.Click += delegate {
+				// this is where you tell it what to do when the button is pressed
+			};
+
+			sign_me_up_link.Click += delegate {
+				// this is where you tell it what to do when the link is pressed
+			};
 
 		}
 	}
