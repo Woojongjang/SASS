a0fb2a02700aaa6baea165bbf1cc27e273aa3b53
Kourosh Khoylou <kouroshkhoylou@Kouroshs-MacBook-Pro.local>
2015-05-08 13:30:51 -0700

    ADDED WRITEMESSAGEACTIVITIY AND MESSAGEBOARD

---
+++ KOTUS/KOTUS/messageBoard.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Application
+{
+	[Activity (Label = "messageBoard")]			
+	public class messageBoard : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			private EditText message;
+
+			private Button writeMessage;
+
+			protected override void OnCreate (Bundle bundle)
+			{
+				base.OnCreate (bundle);
+
+				// set the current layout
+				SetContentView (Resource.Layout.signUp);
+
+				writeMessage = FindViewById<Button> (Resource.Id.button1);
+
+				writeMessage.Click += delegate {
+					// implement here
+
+					StartActivity(typeof(writeMessageActivity));
+				};
+
+			}
+		}
+	}
+}
+
