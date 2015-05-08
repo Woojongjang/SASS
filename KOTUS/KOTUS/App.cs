8c4b0162526937cef7c0ed7e7d020b4954ffbda7
Nicholas G <nlg@ucsd.edu>
2015-05-08 12:41:42 -0700

    NLG: Oops looks like I had broken the code, my bad yall

--- KOTUS/KOTUS/App.cs
+++ KOTUS/KOTUS/App.cs
@@ -3,8 +3,6 @@ using Android.App;
 using Android.Runtime;
 using Parse;
 
-// placed parse initialize in the splash loading activity so that it is initialized when the app opens.. leaving this file here for now until ready for removal
-/*
 namespace KOTUS
 {
 	[Application]
@@ -20,10 +18,9 @@ namespace KOTUS
 
 			// Initialize the Parse client with your Application ID and .NET Key found on
 			// your Parse dashboard
+			Console.WriteLine("Initializing Parse Client");
 			ParseClient.Initialize ("essILhBBMMu4mcWdHW7Qtb4pZJdBU415083KdFXT", "qMWZn2Zjmln0OIMXjJltHBR3HW7cD1yR5J2IVdvk");
-
 
 		}
 	}
 }
-*/
--- KOTUS/KOTUS/SplashActivity.cs
+++ KOTUS/KOTUS/SplashActivity.cs
@@ -12,21 +12,16 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 
-using Parse;
-
 namespace KOTUS
 {
 	[Activity (Theme = "@style/Theme.Splash", MainLauncher = true, NoHistory = true)]			
 	public class SplashActivity : Activity
 	{
-		public SplashActivity (IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
-		{
-		}
+
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
 
-			ParseClient.Initialize ("essILhBBMMu4mcWdHW7Qtb4pZJdBU415083KdFXT", "qMWZn2Zjmln0OIMXjJltHBR3HW7cD1yR5J2IVdvk");
 			Thread.Sleep(2500); // Simulate a long loading process on app startup.
 			// when we have a lot of data to load or uncompress we can replace the thread sleep with that instead
 			StartActivity(typeof(WelcomeActivity));
